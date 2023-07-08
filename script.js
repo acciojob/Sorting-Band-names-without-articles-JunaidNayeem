b595429c31335173d67b6895a7ed8920e97330e2
JunaidNayeem <78254107+JunaidNayeem@users.noreply.github.com>
2023-07-08 19:07:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
+// script.js
+
+let bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Rolling Stones', 'The Who', 'Aerosmith'];
+
+// Remove articles from band names and convert to lowercase for comparison
+let bandNamesWithoutArticles = bandNames.map(name => name.replace(/^(a |an |the )/i, '').toLowerCase());
+
+// Sort the band names in lexicographic order
+bandNamesWithoutArticles.sort();
+
+// Display the sorted band names in the <ul id="band"> tag
+let bandListElement = document.getElementById('band');
+bandNamesWithoutArticles.forEach(name => {
+    let listItem = document.createElement('li');
+    listItem.textContent = bandNames[bandNamesWithoutArticles.indexOf(name)];
+    bandListElement.appendChild(listItem);
+});
 
